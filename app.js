e09024f5797d39b93d58c78e16fdff9d89bbe197
Shahzaib2007 <116277971+Shahzaib2007@users.noreply.github.com>
2022-12-05 16:34:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+function abc(a){
+    a.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPLYcbzkS-PM1-LjzvQ_p5kzquwQX9dBTJWmZoF9jcsUbs9vWfeoxKvyrzwOVCeBNy2cI&usqp=CAU"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
